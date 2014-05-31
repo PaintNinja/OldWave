83d12615a5a06528cf190927066468274291cb93
PaintNinja <paintninjamail-projects@yahoo.co.uk>
2014-05-31 16:44:58 +0200

    Add TestDev webapp
    Adds the TestDev webapp to the WaveAPI cloud. Similar to the Test webapp
    but also shows any arguments the webapp receives as well as some other
    minor stuff.

---
+++ api/apps/TestDev.cmd
@@ -0,0 +1,12 @@
+@echo off
+title WaveAPI TestDev
+echo Test Dev
+echo -------------------------------------------------------------------------------
+echo.
+echo WaveAPI is installed
+echo WaveLocalAPI is installed
+echo.
+echo Arguments recieved: %1
+echo.
+pause
+exit
