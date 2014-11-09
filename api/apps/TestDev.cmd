16aaf1a474e5deb6e6789831e438ab7168fff8de
PaintNinja <paintninjamail-projects@yahoo.co.uk>
2014-11-09 21:06:33 +0100

    Updated TestDev
    Somehow I let it stay ancient from the v0.30 era yet updated it locally
    O.o

--- api/apps/TestDev.cmd
+++ api/apps/TestDev.cmd
@@ -1,12 +1,9 @@
 @echo off
-title WaveAPI TestDev
+title Wave TestDev
 echo Test Dev
 echo -------------------------------------------------------------------------------
 echo.
-echo WaveAPI is installed
-echo WaveLocalAPI is installed
-echo.
-echo Arguments recieved: %1
+echo Arguments recieved: %*
 echo.
 pause
 exit
