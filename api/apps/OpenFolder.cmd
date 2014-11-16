fc78f5739bed6115043d2f286d4d0305bf308ccb
PaintNinja <paintninjamail-projects@yahoo.co.uk>
2014-11-16 21:01:02 +0100

    Made "OpenFolder" webapp
    This webapp opens a specific folder on the PC it is run on. If the
    folder doesn't exist, it'll let you know!

---
+++ api/apps/OpenFolder.cmd
@@ -0,0 +1,46 @@
+@echo off
+SET VersionInteger=1.00
+SET Version=Stable v%VersionInteger%
+SET Title=OpenFolder                                                        %Version%
+SET ErrorTitle=OpenFolder Error                                                 %Version%
+SET TitleBar================================================================================
+SET SubTitleBar=-------------------------------------------------------------------------------
+Title=OpenFolder - %Version%
+if "%1"=="" (
+goto Welcome
+) else (
+goto OpenFolder
+)
+:Welcome
+cls
+echo %Title% && echo %Titlebar%
+echo.
+echo This tiny Wave webapp opens a specific folder on the PC it is run on, if
+echo it exists, of course. 
+echo.
+echo Run this with the argument of the folder you want to open. If the folder
+echo doesn't exist, we'll let you know.
+echo.
+echo Example: Wave:,OpenFolder,23,%%ProgramFiles%%
+echo will open the %%ProgramFiles%% folder
+echo.
+echo Tip! Use %%Username%% for stuff like C:\Users\%%Username%%\Documents for
+echo example so that you can open up the "Documents" folder on the current user
+echo even if you don't know the username.
+echo.
+pause
+exit
+:OpenFolder
+if not exist "%1" (
+cls
+echo %ErrorTitle% && echo %TitleBar%
+echo.
+echo Whoops! I couldn't find the folder or directory:
+echo %1
+echo ...so I was unable to open it.
+pause
+exit
+) else (
+start explorer.exe %1
+exit
+)
