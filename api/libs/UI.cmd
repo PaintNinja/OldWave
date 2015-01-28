d3b1cb48efc18899ddc0838b4d8a49db7e64934a
PaintNinja <paintninjamail-projects@yahoo.co.uk>
2015-01-28 18:25:31 +0100

    Wave Beta v0.60 release! :D
    + Made a welcome screen for Wave :)
    ++ NEW Wave store. Find any Wave webapp, browse the store, try webapps
    and install them - elegently, and without any command.
    +* Wave now makes use of nested if statements and cd commands where
    possible. This results in less code and a more optimised app. :)
    +* Wave now uses indentation in the source code to make it easier to
    maintain.
    +* Better, more descriptive error messages and handling
    +* Slightly lower filesize (source code of Wave v0.59: 14.5KB. Source
    code of Wave v0.60: 13.9KB)
    +*- Major internal changes
    + Added (un)installation support for webapps :D
    + You now only need to enter your beta invitation password once. After
    that, Wave will not need any password :)
    + Wave will let you know what's going on in simple words in a fancy GUI
    in order to make it feel quicker. Stuff such as "Verifying..." and
    "Downloading (webapp name)..." now popup unintrusively when necessary.
    + Added Wprompt and Wbusy libraries
    * Updated UI.cmd library to add support for shorthand Wbusy calling
    * Fixed a security issue where the app.txt would not delete instantly
    after use if the webapp was compressed
    * Fixed major bugs with the "Priority: x" support for .webapp files
    * Changed description of the Wave.exe file
    * The installer now also includes libraries such as UnRAR and 7zip so
    they get installed with Wave rather than during Wave's first launch
    (resulting in better first-launch performance)
    - Wave no longer echo's un-necessary stuff to the console
    - Webapp files are no longer echo'ed to the console
    - Dropped support for the md5sums library
    +*- Anything else I may have forgot to add to the changelog >.<

--- api/libs/UI.cmd
+++ api/libs/UI.cmd
@@ -15,16 +15,22 @@ if "%1"=="ErrorTitleBar" (
   echo.
   goto:eof
   )
+if "%1"=="Wbusy" (
+  start %Wave%\libs\Wbusy.exe %2 %3 /marquee
+  goto:eof
+  )
 :Help
 echo Wave UI Library
 echo ===============================================================================
 echo.
-echo Usage: call %%Wave%%\libs\UI.cmd (Title/SubTitle/ErrorTitle)
+echo Usage: call %%Wave%%\libs\UI.cmd (Title/SubTitle/ErrorTitle/Wbusy)
 echo.
 echo Examples:
 echo - call %%Wave%%\libs\UI.cmd,TitleBar
 echo - call %%Wave%%\libs\UI.cmd,SubTitleBar
 echo - call %%Wave%%\libs\UI.cmd,ErrorTitleBar
+echo - call %%Wave%%\libs\UI.cmd,Wbusy,"Title","Text"
+echo - call %%Wave%%\libs\Wbusy.exe,"Title",/stop
 echo.
 pause
 goto:eof
